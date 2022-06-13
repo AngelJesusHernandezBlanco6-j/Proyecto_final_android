2355a06ce015363860c40b6d0c856b285814d5b6
FlutLab.io <>
2022-06-13 04:42:06 +0000

    Autosave

--- lib/confirmacion.dart
+++ lib/confirmacion.dart
@@ -62,13 +62,3 @@ class Confirmacion extends StatelessWidget {
     );
   }
 }
-
-class PhotoItem {
-  final String image;
-  final String name;
-  PhotoItem(this.image, this.name);
-  final List<PhotoItem> _items = [
-    PhotoItem("https://images.pexels.com/photos/1772973/pexels-photo-1772973.png?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Stephan Seeber"),
-    PhotoItem("https://images.pexels.com/photos/1758531/pexels-photo-1758531.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Liam Gant"),
-  ];
-}
