b7106be26363749af8c242ad51e487d14f4db2f2
FlutLab.io <>
2022-06-13 05:41:41 +0000

    Autosave

--- lib/gridviewequipo.dart
+++ lib/gridviewequipo.dart
@@ -20,21 +20,21 @@ class PhotoItem {
 
 class Gridviewequipo extends StatelessWidget {
   final List<PhotoItem> _items = [
-    PhotoItem("https://images.pexels.com/photos/1772973/pexels-photo-1772973.png?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Stephan Seeber"),
-    PhotoItem("https://images.pexels.com/photos/1758531/pexels-photo-1758531.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Liam Gant"),
-    PhotoItem("https://images.pexels.com/photos/1130847/pexels-photo-1130847.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Stephan Seeber"),
-    PhotoItem("https://images.pexels.com/photos/45900/landscape-scotland-isle-of-skye-old-man-of-storr-45900.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Pixabay"),
-    PhotoItem("https://images.pexels.com/photos/165779/pexels-photo-165779.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Scott Webb"),
-    PhotoItem("https://images.pexels.com/photos/548264/pexels-photo-548264.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Krivec Ales"),
-    PhotoItem("https://images.pexels.com/photos/188973/matterhorn-alpine-zermatt-mountains-188973.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Pixabay"),
-    PhotoItem("https://images.pexels.com/photos/795188/pexels-photo-795188.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Melanie Wupper"),
-    PhotoItem("https://images.pexels.com/photos/5222/snow-mountains-forest-winter.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Jaymantri"),
-    PhotoItem("https://images.pexels.com/photos/789381/pexels-photo-789381.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Riciardus"),
-    PhotoItem("https://images.pexels.com/photos/326119/pexels-photo-326119.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Pixabay"),
-    PhotoItem("https://images.pexels.com/photos/707344/pexels-photo-707344.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Eberhard"),
-    PhotoItem("https://images.pexels.com/photos/691034/pexels-photo-691034.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Mirsad Mujanovic"),
-    PhotoItem("https://images.pexels.com/photos/655676/pexels-photo-655676.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Vittorio Staffolani"),
-    PhotoItem("https://images.pexels.com/photos/592941/pexels-photo-592941.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Tobi"),
+    PhotoItem("https://raw.githubusercontent.com/AngelJesusHernandezBlanco6-j/IMAGENESPROYECTOFINAL/main/EQUIPOSLOGO/FCJUAREZ.jpg", "BRAVOS FC"),
+    PhotoItem(" https://raw.githubusercontent.com/AngelJesusHernandezBlanco6-j/IMAGENESPROYECTOFINAL/main/EQUIPOSLOGO/ATLAS.jpg", "ATLAS"),
+    PhotoItem("https://raw.githubusercontent.com/AngelJesusHernandezBlanco6-j/IMAGENESPROYECTOFINAL/main/EQUIPOSLOGO/CHIVASGDL.jpg", "CHIVAS"),
+    PhotoItem("https://raw.githubusercontent.com/AngelJesusHernandezBlanco6-j/IMAGENESPROYECTOFINAL/main/EQUIPOSLOGO/TIGRES.jpg", "TIGRES"),
+    PhotoItem("https://raw.githubusercontent.com/AngelJesusHernandezBlanco6-j/IMAGENESPROYECTOFINAL/main/EQUIPOSLOGO/atalante.png", "ATLANTE"),
+    PhotoItem("https://raw.githubusercontent.com/AngelJesusHernandezBlanco6-j/IMAGENESPROYECTOFINAL/main/EQUIPOSLOGO/delfines.jpg", "ATLETICO COATZACOALCOS"),
+    PhotoItem("https://raw.githubusercontent.com/AngelJesusHernandezBlanco6-j/IMAGENESPROYECTOFINAL/main/EQUIPOSLOGO/hidalgo.jpg", "ATLETICO HIDALGO"),
+    PhotoItem("https://raw.githubusercontent.com/AngelJesusHernandezBlanco6-j/IMAGENESPROYECTOFINAL/main/EQUIPOSLOGO/irapuato.png", "IRAPUATO"),
+    PhotoItem("https://raw.githubusercontent.com/AngelJesusHernandezBlanco6-j/IMAGENESPROYECTOFINAL/main/EQUIPOSLOGO/leon.png", "LEON"),
+    PhotoItem("https://raw.githubusercontent.com/AngelJesusHernandezBlanco6-j/IMAGENESPROYECTOFINAL/main/EQUIPOSLOGO/monarca.png", "MONARCAS"),
+    PhotoItem("https://raw.githubusercontent.com/AngelJesusHernandezBlanco6-j/IMAGENESPROYECTOFINAL/main/EQUIPOSLOGO/murcielagos.jpg", "MURCIELAGOS DE GUAMUCHIL"),
+    PhotoItem("https://raw.githubusercontent.com/AngelJesusHernandezBlanco6-j/IMAGENESPROYECTOFINAL/main/EQUIPOSLOGO/pachuca.jpg", "PACHUCA"),
+    PhotoItem("https://raw.githubusercontent.com/AngelJesusHernandezBlanco6-j/IMAGENESPROYECTOFINAL/main/EQUIPOSLOGO/queretaro.png", "QUERETARO"),
+    PhotoItem("https://raw.githubusercontent.com/AngelJesusHernandezBlanco6-j/IMAGENESPROYECTOFINAL/main/EQUIPOSLOGO/tepic.png", "TEPIC"),
+    PhotoItem("https://raw.githubusercontent.com/AngelJesusHernandezBlanco6-j/IMAGENESPROYECTOFINAL/main/EQUIPOSLOGO/tijuana.png", "TIJUANA"),
   ];
 
   @override
